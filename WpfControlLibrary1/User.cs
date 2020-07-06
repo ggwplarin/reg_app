0eb761e074b73c61f7e4a651a935522f5b1bbc45
ggwplarin <al8137962@gmail.com>
2020-07-06 19:02:06 +0300

    ¯\_(ツ)_/¯

---
+++ WpfControlLibrary1/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfControlLibrary1
+{
+    [Serializable]
+    class User
+    {
+
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string SecretQuestion { get; set; }
+        public string SecretAnswer { get; set; }
+        public string EMail { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Passport { get; set; }
+
+        public User(string login, string password, string secretQuestion, string secretAnswer, string eMail, string phoneNumber, string passport)
+        {
+            Login = login;
+            Password = password;
+            SecretQuestion = secretQuestion;
+            SecretAnswer = secretAnswer;
+            EMail = eMail;
+            PhoneNumber = phoneNumber;
+            Passport = passport;
+        }
+    }
+}
--- WpfControlLibrary1/UserControl1.xaml.cs
+++ WpfControlLibrary1/UserControl1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+using Path = System.IO.Path;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace WpfControlLibrary1
 {
@@ -22,7 +26,8 @@ namespace WpfControlLibrary1
     public partial class UserControl1 : UserControl
     {
         bool[] oks = new bool[8] { false, false, false, false, false, false, false, false };
-        
+        List<User> users = new List<User>();
+        string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory), "users.dat");
         public UserControl1()
         {
             InitializeComponent();
@@ -51,12 +56,12 @@ namespace WpfControlLibrary1
             else { RepeatPasswBox.Foreground = new SolidColorBrush(Colors.Red); oks[2] = false; }
         }
 
-        private void SectetQBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void SecretQBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             oks[3] = true;
         }
 
-        private void SectetABox_TextChanged(object sender, TextChangedEventArgs e)
+        private void SecretABox_TextChanged(object sender, TextChangedEventArgs e)
         {
             if (((TextBox)sender).Text.Length > 0) { oks[4] = true; }
             else { oks[4] = false; }
@@ -90,6 +95,85 @@ namespace WpfControlLibrary1
             else { PassportBox.Foreground = new SolidColorBrush(Colors.Red); oks[7] = false; }
         }
 
+        private void ConfirmBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (oks.All(x => x))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        if (fs.Length > 0)
+                        {
+                            users = (List<User>)formatter.Deserialize(fs);
+                        }
+                    }
+                    if (!users.Any(u => u.Login == LoginBox2.Text && u.Password == PasswBox2.Password))
+                    {
+                        users.Add(new User(LoginBox.Text,
+                            PasswBox.Password,
+                            SecretQBox.Text,
+                            SecretABox.Text,
+                            EMailBox.Text,
+                            PhoneBox.Text,
+                            PassportBox.Text));
 
+
+                        using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                        {
+                            formatter.Serialize(fs, users);
+                        }
+                    }
+                    MessageBox.Show($"Hello {LoginBox.Text}!");
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Something went wrong");
+            }
+
+        }
+
+        private void Confirm2Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(PasswBox2.Password) && !string.IsNullOrEmpty(LoginBox2.Text))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        if (fs.Length > 0)
+                        {
+                            users = (List<User>)formatter.Deserialize(fs);
+                        }
+                    }
+                    if (users.Any(u => u.Login == LoginBox2.Text && u.Password == PasswBox2.Password))
+                    {
+                        MessageBox.Show($"Hello {LoginBox2.Text}!");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Go away!");
+                    }
+
+                }
+            }
+            catch
+            {
+                MessageBox.Show($"Something went wrong");
+            }
+        }
+
+        private void PasswBox2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LoginBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
